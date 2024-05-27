26d3665afe8ec7e0390fcfe1711f87155f11ae1d
NxtNinja <136341993+NxtNinja@users.noreply.github.com>
2024-05-27 18:42:49 +0530

    meta description and title added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import HomePage from "@/components/HomePage";
 import Layout from "@/components/Layout";
+import Head from "next/head";
 
 const index = () => {
   return (
     <>
+      <Head>
+        <title>Priyangsu Banik</title>
+        <meta name="description" content="Welcome to my Portfolio" />
+        <meta
+          name="keywords"
+          content="Next.js developer, React developer, Typescript, JavaScript"
+        />
+      </Head>
       <Layout>
         <HomePage />
       </Layout>
